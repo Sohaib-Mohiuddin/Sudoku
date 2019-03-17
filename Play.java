8535e2e2439e3edb80a07da5e5ffdfe333846e06
Sohaib Mohiuddin <sohaib.mohiuddin@uoit.net>
2019-03-17 19:22:03 -0400

    update

--- Play.java
+++ Play.java
@@ -4,15 +4,21 @@ import java.awt.font.TextAttribute;
 import javax.swing.*;
 import java.util.Random; import java.util.Map; import java.util.HashMap;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DnDConstants;
+
 public class Play extends JFrame {
 
     public JFrame frame;
-    public JPanel panel1, panel2;
-    public JButton Options, Save, Return;
-    public JToggleButton Hint;
+    public JPanel panel1, num_panel, timer_panel;
+    public JButton return_button, help, one, two, three, four, five, six, seven, eight, nine;
+    public JToggleButton hint;
     public JLabel title_play, label2;
-    public JMenu menu, submenu;
-    public JMenuItem i1, i2, i3, i4, i5;
+    public JMenuBar menubar;
+    public JMenu menu_file, submenu;
+    public JMenuItem save, item_options;
 
     public static final int GRID_SIZE = 9;
     public static final int SUBGRID_SIZE = 3;
@@ -23,6 +29,7 @@ public class Play extends JFrame {
     public static final Color UNCLICKED_BOX = Color.white;
     public static final Color CLICKED_BOX = Color.CYAN;
     public static final Font FONT_NUMBERS = new Font("Comic Sans MS", Font.BOLD, 20);
+    public static final Font BUTTON_FONTS = new Font("Comic Sans MS", Font.BOLD, 15);
     public static final Font TITLE_FONTS = new Font("Comic Sans MS", Font.BOLD, 50);
 
     private int previousRowPicked;
@@ -35,7 +42,10 @@ public class Play extends JFrame {
     private boolean[][] mask = maskGenerator();
 
     private JTextField[][] cells = new JTextField[GRID_SIZE][GRID_SIZE];
+    private JButton[][] nums = new JButton[SUBGRID_SIZE][SUBGRID_SIZE];
     JPanel cell_panels = new JPanel();
+
+    //final String propertyName = "text";
 
     // public Play() {
     // }
@@ -48,12 +58,39 @@ public class Play extends JFrame {
         frame.setLayout(null);
         frame.getContentPane().setBackground(Color.cyan);
 
+        menubar = new JMenuBar();
+        menu_file = new JMenu("File");
+        item_options = new JMenuItem("Options");
+        save = new JMenuItem("Save");
+        menu_file.add(save);
+        menubar.add(menu_file);
+        menubar.add(item_options);
+
         title_play = new JLabel("Sudoku-sama");
         title_play.setBounds(610, 100, 340, 70);
         title_play.setFont(TITLE_FONTS);
         Map<TextAttribute, Object> attributes = new HashMap<>(TITLE_FONTS.getAttributes());
         attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
         title_play.setFont(TITLE_FONTS.deriveFont(attributes));
+
+        return_button = new JButton("Return to Main Menu");
+        return_button.setFont(BUTTON_FONTS);
+        return_button.setBounds(1250, 860, 200, 50);
+
+        help = new JButton("Help");
+        help.setFont(BUTTON_FONTS);
+        help.setBounds(115, 450, 200, 50);
+
+        hint = new JToggleButton("Hint: Default -> ON");
+        hint.setFont(BUTTON_FONTS);
+        hint.setBounds(115, 550, 200, 50);
+        hint.setSelected(true);
+
+        num_panel = new JPanel();
+        num_panel.setBackground(Color.PINK);
+        num_panel.setLayout(new GridLayout(3, 3));
+        num_panel.setBounds(1100, 200, CELL_SIZE*3, CELL_SIZE*3);
+        num_panel.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.black));
 
         panel1 = new JPanel();
         panel1.setBackground(Color.PINK);
@@ -90,11 +127,13 @@ public class Play extends JFrame {
                 }
 
                 int user_input = Integer.parseInt(cells[rowPicked][colPicked].getText());
-                if (user_input == puzzle[rowPicked][colPicked]) {
-                    cells[rowPicked][colPicked].setBackground(RIGHT_ANSWER);
-                    cells[rowPicked][colPicked].setEditable(false);
-                } else {
-                    cells[rowPicked][colPicked].setBackground(WRONG_ANSWER);
+                if(hint.isSelected()) {
+                    if (user_input == puzzle[rowPicked][colPicked]) {
+                        cells[rowPicked][colPicked].setBackground(RIGHT_ANSWER);
+                        cells[rowPicked][colPicked].setEditable(false);
+                    } else {
+                        cells[rowPicked][colPicked].setBackground(WRONG_ANSWER);
+                    }
                 }
 
                 previousRowPicked = rowPicked;
@@ -102,11 +141,66 @@ public class Play extends JFrame {
             }
         };
 
+        Action button_action = new AbstractAction(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int rowPicked_button = -1;
+                int colPicked_button = -1;
+
+                JButton button_source = (JButton)e.getSource();
+
+                boolean found_button = false;
+
+                for (int i = 0; i < SUBGRID_SIZE && !found_button; ++i) {
+                    for (int j = 0; j < SUBGRID_SIZE && !found_button; ++j) {
+                        if(nums[i][j] == button_source) {
+                            rowPicked_button = i;
+                            colPicked_button = j;
+                            //System.out.println(nums[i][j].getText());
+                            //cells[i][j].setText(nums[i][j].getText());
+                            
+                            found_button = true;   
+                        }
+                    }
+                }
+            }
+        };
+
+        int button_numbers = 1;
+        
+        for (int i = 0; i < SUBGRID_SIZE; ++i) {
+            for (int j = 0; j < SUBGRID_SIZE; ++j) {
+                nums[i][j] = new JButton(button_numbers + "");
+                nums[i][j].setBorder(BorderFactory.createLineBorder(Color.black));
+                
+                /**
+                 * FIXME:
+                 * Fix dragging values from number panel
+                 */
+                nums[i][j].addActionListener(button_action);
+                int button_user_input = Integer.parseInt(nums[i][j].getText());
+                nums[i][j].setTransferHandler(new ValueExportTransferHandler(Integer.toString(button_user_input)));
+
+                nums[i][j].addMouseMotionListener(new MouseAdapter() {
+                    @Override
+                    public void mouseDragged(MouseEvent e) {
+                        JButton button = (JButton) e.getSource();
+                        TransferHandler handle = button.getTransferHandler();
+                        handle.exportAsDrag(button, e, TransferHandler.COPY);
+                    }
+                });
+                button_numbers++;
+                num_panel.add(nums[i][j]);
+
+            }
+        }
+
         for (int i = 0; i < GRID_SIZE; ++i) {
             for (int j = 0; j < GRID_SIZE; ++j) {
 
                 cells[i][j] = new JTextField();
                 cells[i][j].setBorder(BorderFactory.createLineBorder(Color.black));
+                cells[i][j].setTransferHandler(new ValueImportTransferHandler());
                 cells[i][j].addActionListener(action);
 
                 if (i % 3 == 0 && i != 0){
@@ -146,10 +240,19 @@ public class Play extends JFrame {
         }
 
         frame.getContentPane().add(title_play);
+        frame.setJMenuBar(menubar);
+        frame.add(return_button);
+        frame.add(help);
+        frame.add(hint);
         frame.getContentPane().add(panel1);
+        frame.getContentPane().add(num_panel);
 
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
+    }
+
+    public void setSelected(boolean b) {
+        b = true;
     }
 
     public boolean[][] maskGenerator() {
@@ -172,7 +275,72 @@ public class Play extends JFrame {
 
     public static void main(String[] args) {
         Play play = new Play();
-
         play.GUI();
+    }
+
+    public static class ValueExportTransferHandler extends TransferHandler {
+
+        public static final DataFlavor SUPPORTED_DATE_FLAVOR = DataFlavor.stringFlavor;
+        private String value;
+
+        public ValueExportTransferHandler(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public int getSourceActions(JComponent c) {
+            return DnDConstants.ACTION_COPY_OR_MOVE;
+        }
+
+        @Override
+        protected Transferable createTransferable(JComponent c) {
+            Transferable t = new StringSelection(getValue());
+            return t;
+        }
+
+        @Override
+        protected void exportDone(JComponent source, Transferable data, int action) {
+            super.exportDone(source, data, action);
+            // Decide what to do after the drop has been accepted
+        }
+
+    }
+
+    public static class ValueImportTransferHandler extends TransferHandler {
+
+        public static final DataFlavor SUPPORTED_DATE_FLAVOR = DataFlavor.stringFlavor;
+
+        public ValueImportTransferHandler() {
+        }
+
+        @Override
+        public boolean canImport(TransferHandler.TransferSupport support) {
+            return support.isDataFlavorSupported(SUPPORTED_DATE_FLAVOR);
+        }
+
+        @Override
+        public boolean importData(TransferHandler.TransferSupport support) {
+            boolean accept = false;
+            if (canImport(support)) {
+                try {
+                    Transferable t = support.getTransferable();
+                    Object value = t.getTransferData(SUPPORTED_DATE_FLAVOR);
+                    if (value instanceof String) {
+                        Component component = support.getComponent();
+                        if (component instanceof JLabel) {
+                            ((JLabel) component).setText(value.toString());
+                            accept = true;
+                        }
+                    }
+                } catch (Exception exp) {
+                    exp.printStackTrace();
+                }
+            }
+            return accept;
+        }
     }
 }
